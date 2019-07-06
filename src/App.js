260bd1d2dbe95ae29f512f7e6e272c407f70b23c
Richard Goh <richard.goh@singtel.com>
2019-07-06 15:49:25 +0800

    Static driver card

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import React from 'react';
 import DriverCard from "./components/DriverCard"
 
 import './App.css';
-import './screen.css'
+import './screen.css';
+
+let driverPhotoUrl = "https://scontent-sin6-2.xx.fbcdn.net/v/t1.0-9/43522912_10205230133270116_181260028051718144_n.jpg?_nc_cat=110&_nc_oc=AQk4u4_YjgGHb1iFp0MuP6CNEieBn2m9BWSo1f2JcgmLXqUsK_JnhYPeZbGqipnGzzg&_nc_ht=scontent-sin6-2.xx&oh=19add1810f487db9e3e80dd06efc0e7f&oe=5DC63E9B"
+{/* <img src="./assets/images/driver_1_matthew.jpg" /> */}
 
 function App() {
   return (
     <div className="App">
-      <DriverCard name="James" riskLvl="9"  hasPassenger="yes" currSpeed="90km/h"/> 
+      <DriverCard driverPhotoUrl={driverPhotoUrl} name="Handsome Matthew" riskLvl="9"  passengerName="Jennifer" currSpeed="120km/h" numActiveHours="6 hours"/> 
     </div>
   );
 }
--- src/components/DriverCard.jsx
+++ src/components/DriverCard.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 
+
 export class DriverCard extends Component {
     render(props) {
         return (
@@ -7,16 +8,18 @@ export class DriverCard extends Component {
                 <div className="container driver-card ">
                     <div className="row">
                         <div className="col-md-2">
-                            <div className="">
-                                
+                            <div className="driver-photo-container">
+                                <img src={this.props.driverPhotoUrl} />
                             </div>
                         </div>
-                        <div className="col-md-10">
-                            <p className="driver-name">{this.props.name} </p>
-                            <p> {this.props.riskLvl} </p>
-                            <p>{this.props.hasPassenger}  </p>
-                            <p>{this.props.currentSpeed} </p>
-                            
+                        <div className="col-md-5">
+                            <p className="driver-name">Driver name: {this.props.name} </p>
+                            <p> Ferrying Passenger: {this.props.passengerName}  </p>
+                            <p>Current Speed: {this.props.currSpeed} </p>
+                        </div>
+                        <div className="col-md-5">
+                            <p> Risk Level: {this.props.riskLvl} </p>
+                            <p>Number of active hours: {this.props.numActiveHours} </p>
                         </div>
                     </div>
                 </div>
