c7c073ca28a216b31e0ba4d602e6635aecd2def6
Richard Goh <richard.goh@singtel.com>
2019-07-06 20:05:51 +0800

    Improved driver card

--- src/components/DriverCard.jsx
+++ src/components/DriverCard.jsx
@@ -6,26 +6,31 @@ export class DriverCard extends Component {
         return (
             <div>
                 {
-                    this.props.riskLvl>2 ?
+                    this.props.riskLvl > 2 ?
                         <div>
                             <div className="container driver-card ">
                                 <div className="row">
-                                    <div className="col-md-2">
+                                    <div className="col-md-2 centered-container">
                                         <div className="driver-photo-container">
                                             <img alt="driver" src={this.props.driverPhotoUrl} />
                                         </div>
                                     </div>
-                                    <div className="col-md-4">
+                                    <div className="col-md-4 centered-container">
                                         <p>Driver name: {this.props.name} </p>
                                         <p> Ferrying Passenger: {this.props.passengerName}  </p>
                                         <p>Current Speed: {this.props.currSpeed} </p>
                                     </div>
-                                    <div className="col-md-4">
-                                       <p>Number of active hours: {this.props.numActiveHours} </p>
+                                    <div className="col-md-4 centered-container">
+                                        <p className="text-center"> Risk Level: </p>
+                                        <p className="text-center risk-level">{this.props.riskLvl} </p>
+                                        <p>Number of active hours: {this.props.numActiveHours} </p>
                                     </div>
-                                    <div className="col-md-2">
-                                        <p> Risk Level: {this.props.riskLvl} </p>
-                                        
+                                    <div className="col-md-2 centered-container">
+
+                                        <button className="btn mb-3 btn-primary"> Send Alert </button>
+
+
+
                                     </div>
                                 </div>
                             </div>
