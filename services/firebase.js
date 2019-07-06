394124b4d28891a0d0ff48ba7bc1fd759e2685ae
Matthew Alexander <alexandermatthew12@gmail.com>
2019-07-06 13:57:33 +0800

    added dotenv & firebase

---
+++ services/firebase.js
@@ -0,0 +1,24 @@
+const dotenv = require('dotenv');
+
+require('dotenv').config({
+    path: '../.env'
+});
+
+const firebase = require('firebase');
+
+const firebaseConfig = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID
+  };
+
+// Initialize Firebase
+const Firebase = firebase.initializeApp(firebaseConfig);
+
+module.exports = {
+    Firebase
+};
